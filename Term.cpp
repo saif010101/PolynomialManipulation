511c9476fcb05a3d1bf3f92044c9c1b49bae7961
Muhammad Saif <147891557+saif010101@users.noreply.github.com>
2024-05-02 19:34:10 +0500

    Update Term.cpp

--- Term.cpp
+++ Term.cpp
@@ -42,8 +42,33 @@ Term Term::integrate()
     return Term(0.0,0); // returns a zero term
 }
 
-// 5x^2
-// 10 x
+void Term::display()
+{
+        // if a term's exponent is non-zero
+        if (exp > 0)
+        {
+            if (coff > 0 && exp == 1)   // Note 2 : if exponent is one it means x is raised to the power one which we don;t usually write so that's why no '^' is used
+                cout << " + " << coff << "x";
+            else if (coff < 0 && exp == 1)
+                cout << " - " << -coff << "x";  // // Note 1 : we multiplied a minus because we are printing a " - " our self for extra space before coefficient
+            else if (coff > 0 && exp > 1)
+                cout << " + " << coff << "x^" << exp;  
+            else if (coff < 0 && exp > 1)
+                cout << " - " << -coff << "x^" << exp; // See Note 1 to know why we multiplied a minus           
+        }
+
+        else // if a term's exponent is zero i.e it is a constant term
+        {
+            
+            if (coff > 0)
+                cout << " + " << coff;
+            else // if (coff < 0)
+                cout << " - " << -coff; // see note 1
+        }
+
+}
+
+
 Term Term::operator*(Term &p2_term)
 {
     // this function multiply object of class Term in such a way that 
@@ -56,4 +81,14 @@ Term Term::operator*(Term &p2_term)
     double p3_exp  = (*this).exponent() + p2_term.exponent();
 
     return Term(p3_coff,p3_exp); // creates an object and initiliases it with values and returns it
+}
+
+Term Term::operator+(Term &p2_term)
+{
+    // Note : Two Algebric terms can only be added if they have the same exponent
+    // example : 2x^3 + 7x^3 hence why exp remains same and coefficient is added
+    int _exp = (*this).exponent();
+    double _coff = (*this).coefficient() + p2_term.coefficient();
+
+    return Term(_coff,_exp);
 }
