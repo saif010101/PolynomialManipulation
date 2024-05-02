131b86a74911048bca6721dc31dc788dcca48c12
Muhammad Saif <147891557+saif010101@users.noreply.github.com>
2024-05-02 19:36:01 +0500

    Update Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -4,9 +4,10 @@
 #include "Term.h"
 
 
+
 class Polynomial
 {
-    private:
+    protected:
         std::vector<Term> terms;	// a dynamic array of 'Term'
            
     public:
@@ -19,8 +20,68 @@ class Polynomial
         void display(); // prints polynomial
         double evaluate(double x0); // evaluates polynomial at x = x0
         void test();
-        Polynomial take_derivative();
+        int degree();  // degree of polynomial
+        Polynomial differentiate();
+        Polynomial integrate();
+
 
         // operators
         Polynomial operator* (Polynomial& p2);
+        Polynomial operator+ (Polynomial& p2);
 };
+
+class Root
+{
+    private:
+        double real;
+        double imag;
+    public:
+        Root(){}
+        Root(double _real,double _imag):real(_real),imag(_imag){}
+
+        void display();
+};
+
+class Cubic : public Polynomial
+{
+    private:
+        Root* roots;
+    public:
+        Cubic(){}
+        Cubic(Term* _terms,int size): Polynomial(_terms,size)
+        {
+
+        } 
+};
+
+class Quadratic: public Polynomial
+{
+    private:
+        double a; double b; double c;
+        Root* roots;
+        int roots_count;
+
+        double discriminant()
+        {
+            std::cout << a << b << c << std::endl;
+            return b*b - 4*a*c;     // discriminant = b^2 - 4ac
+        }
+        void calculateRoots();
+    public:
+        Quadratic():roots(NULL),a(0),b(0),c(0),roots_count(0){}
+        Quadratic(Term* _terms,int size);
+        Quadratic(std::string equation):Polynomial(equation){ }
+
+        // member functions       
+        
+        void displayRoots();
+
+        // destructor
+        ~Quadratic()
+         {
+            delete[] roots;
+         }
+
+        
+};
+
