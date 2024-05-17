3634d859f9d0a45e0364df66e449d35f494b14f4
Muhammad Saif <147891557+saif010101@users.noreply.github.com>
2024-05-17 06:55:59 +0500

    completed

--- Polynomial.h
+++ Polynomial.h
@@ -3,7 +3,26 @@
 #include <string>
 #include "Term.h"
 
+using namespace std;
 
+class Root;
+class Polynomial;
+class Quadratic;
+class Cubic;
+
+class Root
+{
+    public:
+        double real;
+        double imag;
+
+        // constructors
+        Root(){}
+        Root(double _real,double _imag):real(_real),imag(_imag){}
+
+        void display(); 
+
+};
 
 class Polynomial
 {
@@ -13,18 +32,17 @@ class Polynomial
     public:
         // constructors
         Polynomial();   // default constructor
-        Polynomial(Term* values,int size);   // parametrized ctor, intialize Polynomial with given values
         Polynomial(std::string equation);  
 
         // member functions
         void display(); // prints polynomial
         double evaluate(double x0); // evaluates polynomial at x = x0
-        void test();
-        int degree();  // degree of polynomial  
-
-        //   
+        int degree();  // degree of polynomial 
+        
         double integrate(double from,double to); // area under the curve
         double differentiate(double x0); // slope of the tangent line to the curve (*this) at point x = x0   
+
+        vector<Term>& getTerms() {return terms;}
 
 
         // operations
@@ -33,60 +51,52 @@ class Polynomial
         Polynomial operator* (Polynomial& p2);
         Polynomial operator+ (Polynomial& p2);
         Polynomial operator- (Polynomial p2);
+        friend ostream& operator<<(ostream& out,Polynomial& p); 
+
 };
-
-class Root
-{
-    private:
-        double real;
-        double imag;
-    public:
-        Root(){}
-        Root(double _real,double _imag):real(_real),imag(_imag){}
-
-        void display();
-};
-
-class Cubic : public Polynomial
-{
-    private:
-        Root* roots;
-    public:
-        Cubic(){}
-        Cubic(Term* _terms,int size): Polynomial(_terms,size)
-        {
-
-        } 
-};
-
 class Quadratic: public Polynomial
 {
-    private:
-        double a; double b; double c;
-        Root* roots;
-        int roots_count;
-
-        double discriminant()
-        {
-            std::cout << a << b << c << std::endl;
-            return b*b - 4*a*c;     // discriminant = b^2 - 4ac
-        }
-        void calculateRoots();
+       
     public:
-        Quadratic():roots(NULL),a(0),b(0),c(0),roots_count(0){}
-        Quadratic(Term* _terms,int size);
-        Quadratic(std::string equation):Polynomial(equation){ }
+        vector<Root> roots;
 
-        // member functions       
-        
+        // constructors
+        Quadratic():a(0),b(0),c(0){}
+        Quadratic(std::string equation);   
+        Quadratic(vector<Term>& terms);     
+
+        // member functions               
+        void displayRoots();
+        double getDiscriminant() { return discriminant; }
+         
+
+        // data members
+    private:
+        double a = 0.0; 
+        double b = 0.0; 
+        double c = 0.0;   
+        double discriminant;
+        void calculateRoots();
+        void calcDiscriminant();          
+};
+
+class Cubic : public Polynomial     // Cubic is a Polynomial
+{
+    
+    public:
+        vector<Root> roots;
+
+        Cubic(){}
+        Cubic(string equation);
+        Cubic(vector<Term>& terms); 
+
         void displayRoots();
 
-        // destructor
-        ~Quadratic()
-         {           
-            delete[] roots;
-         }
-
-        
+    private:
+        double a = 0.0;
+        double b = 0.0;
+        double c = 0.0;
+        double d = 0.0;
+        void calculateRoots();        
 };
 
